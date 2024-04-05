503502575f36d8385f64d79ac578132662782b80
Maxim Vancompernolle <mhv6@gatech.edu>
2024-04-05 13:46:48 -0400

    modify woodland mansion filter

--- src/main/java/com/mvc/allarthropods/Config.java
+++ src/main/java/com/mvc/allarthropods/Config.java
@@ -6,7 +6,8 @@ public class Config {
     public static final int SEED_MATCHES = 10;
     public static final int LOG_DELAY = 10_000;
     public static final MCVersion VERSION = MCVersion.v1_16_1;
-    public static final double WM_MAX_DIST = 48D * 48D;
-    public static final double RP_MAX_DIST = 6D * 6D;
+    public static final double SPAWN_MAX_DIST = 6D * 6D;
+    public static final double WM_MAX_DIST = 4D;
+    public static final double RP_MAX_DIST = 2D * 2D;
     public static final double MS_MAX_DIST = 10D * 10D;
 }
--- src/main/java/com/mvc/allarthropods/Filters/StructureFilter.java
+++ src/main/java/com/mvc/allarthropods/Filters/StructureFilter.java
@@ -1,8 +1,10 @@
 package com.mvc.allarthropods.Filters;
 
 import com.mvc.allarthropods.Config;
+import com.seedfinding.mcbiome.source.OverworldBiomeSource;
 import com.seedfinding.mccore.rand.ChunkRand;
 import com.seedfinding.mccore.util.pos.CPos;
+import com.seedfinding.mcterrain.terrain.OverworldTerrainGenerator;
 import util.mansionSim.Mansion;
 import util.mansionSim.MansionGenerator;
 import util.mansionSim.MansionPiece;
@@ -12,27 +14,32 @@ public class StructureFilter {
     private final ChunkRand chunkRand;
     public Mansion wm = new Mansion(Config.VERSION);
     public MansionGenerator wmGenerator = new MansionGenerator(Config.VERSION);
+    public OverworldBiomeSource owBiomeSource;
+    public OverworldTerrainGenerator owTerrainGen;
 
     public StructureFilter(long structureSeed, ChunkRand chunkRand) {
         this.structureSeed = structureSeed;
         this.chunkRand = chunkRand;
+        this.owBiomeSource = new OverworldBiomeSource(Config.VERSION, structureSeed);
+        this.owTerrainGen = new OverworldTerrainGenerator(owBiomeSource);
     }
 
     public boolean filterStructures() {
         CPos wmLocation = wm.getInRegion(structureSeed, 0, 0, chunkRand);
 
-        if (wmLocation.getX() > 2 || wmLocation.getZ() > 2) {
+        if (wmLocation.getX() > Config.WM_MAX_DIST || wmLocation.getZ() > Config.WM_MAX_DIST) {
             return false;
         }
 
         wmGenerator.fastGenerate(structureSeed, wmLocation.getX(), wmLocation.getZ(), chunkRand);
-
+        boolean hasFakeEndPortalRoom = false;
 
         for (MansionPiece wmPiece : wmGenerator.getPieces()) {
             if (wmPiece.getTemplate().contains("1x2_s2")) {
-                return true;
+                hasFakeEndPortalRoom = true;
+                break;
             }
         }
-        return false;
+        return hasFakeEndPortalRoom;
     }
 }
